232f2ba554a8ff17ca940418e445478442033c36
Garisian Kana <garisian22@gmail.com>
2016-01-29 10:21:16 -0500

    got basic functionality

--- run_recognition.py
+++ run_recognition.py
@@ -15,14 +15,20 @@ import urllib
 from helpers import *
 
 if __name__ == "__main__":
-    #for i in os.listdir(os.getcwd()+"/uncropped"):
-    #    image = imread(os.getcwd()+"/uncropped/"+i);
+    # Create the empty sets for training, testing and validifying
     trainingSet = {}
     testSet = {}
     validationSet = {}
+
     person_mapping = {"butler":1, "radcliffe":2, "vartan":3, "bracco":4, "gilpin":5, "harmon":6}
     dir_of_cropped = os.getcwd()+"/cropped"
     get_data(dir_of_cropped, trainingSet, testSet, validationSet, person_mapping)
-
-
-
+    
+    count = 1
+    #print len(testSet[1])
+    #print len(trainingSet[1])
+    #print len(validationSet[1])
+    transformed_info = transform_info(trainingSet, testSet, validationSet)
+    #for category in testSet:
+    #    for image in testSet[category]:
+            
